9330f944d22fda6d6827d5770965e18a9a0356bf
Chukwuka Madueke <smadueke@utexas.edu>
2023-08-22 19:52:50 -0500

    Video detail page in developement. Other completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,29 +4,43 @@ import {Paper, IconButton} from "@mui/material";
 import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
-  return (
-    <Paper 
-        component="form"
-        onSubmit={() => {}}
-        sx={{
-            borderRadius: 20,
-            border: '1px solid #e3e3e3',
-            pl: 2,
-            boxShadow: 'none',
-            mr: {sm: 5}
-        }}
-    >
-        <input
-            className="search-bar"
-            placeholder="Search..."
-            value=""
-            onChange={() => {}}
-        />
-        <IconButton type ="submit" sx={{p: '10px', color: 'red'}}>
-            <Search />
-        </IconButton>
-    </Paper>
-  )
+    const [searchTerm, setsearchTerm] = useState('')
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+
+        if(searchTerm) {
+            navigate(`/search/${searchTerm}`)
+
+            setsearchTerm('')
+        }
+
+    }
+    return (
+        <Paper 
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: {sm: 5}
+            }}
+        >
+            <input
+                className="search-bar"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setsearchTerm(e.target.value)}
+            />
+            <IconButton type ="submit" sx={{p: '10px', color: 'red'}}>
+                <Search />
+            </IconButton>
+        </Paper>
+    )
 }
 
 export default SearchBar 
